f791c1d7ad92126e949eb190eb27e2d19c8db447
alexy3 <alexy3@illinois.edu>
2024-09-13 22:16:29 -0500

    finished log file generation

--- mp1/test.go
+++ mp1/test.go
@@ -4,20 +4,25 @@ import (
     "bytes"
     "os/exec"
 	"fmt"
+	"math/rand"
 )
 
 const letters = "abcdefghijklmnopqrstuvwxyz"
-set := make(map[string]struct{})
+var set = make(map[string]struct{})
 
 func generateRandomWord(length int) string {
-	
-	word := make([]byte, length)
-	for i := range word {
-		word[i] = letters[rand.Intn(len(letters))]
-	}
+	for {
+		word := make([]byte, length)
+		for i := range word {
+			word[i] = letters[rand.Intn(len(letters))]
+		}
+		wordStr := string(word)
 
-	set[string(word)] = struct{}{}
-	return string(word)
+		if _, exists := set[wordStr]; !exists {
+			set[wordStr] = struct{}{}
+			return wordStr
+		}
+	}
 }
 
 func main() {
@@ -65,8 +70,8 @@ func main() {
 	// just generates a log with the given text by the caller
 	// we will call and have 5 known line the same across all logs
 	// rare: 1x in 1/5/10 logs
-	// somewhat frequent: 20x in 1/5/10 logs
-	// frequent: 100x in 1/5/10 logs
+	// somewhat frequent: 10x in 1/5/10 logs
+	// frequent: 50x in 1/5/10 logs
 	// Input generation: inputs[10]
 	//  1 2 3 ... 10
 	//  ->
@@ -77,23 +82,63 @@ func main() {
 	for i := 0; i < 10; i++ {
 		logs[i] = append(logs[i], rareAll, "\n")
 	}
-
 	rareSome := generateRandomWord(4)
 	for i := 0; i < 5; i++ {
 		logs[i] = append(logs[i], rareSome, "\n")
 	}
-
 	rareOne := generateRandomWord(4)
 	for i := 0; i < 1; i++ {
 		logs[i] = append(logs[i], rareOne, "\n")
 	}
-
-	// Print the logs to verify
-	for i, log := range logs {
-		fmt.Printf("logs[%d]: %s\n", i, log)
+	freqAll := generateRandomWord(4)
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 50; j++ {
+			logs[i] = append(logs[i], freqAll, "\n")
+		}
+	}
+	freqSome := generateRandomWord(4)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 50; j++ {
+			logs[i] = append(logs[i], freqSome, "\n")
+		}
+	}
+	freqOne := generateRandomWord(4)
+	for i := 0; i < 1; i++ {
+		for j := 0; j < 50; j++ {
+			logs[i] = append(logs[i], freqOne, "\n")
+		}
+	}
+	somewhatFreqAll := generateRandomWord(4)
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			logs[i] = append(logs[i], somewhatFreqAll, "\n")
+		}
+	}
+	somewhatFreqSome := generateRandomWord(4)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 10; j++ {
+			logs[i] = append(logs[i], somewhatFreqSome, "\n")
+		}
+	}
+	somewhatFreqOne := generateRandomWord(4)
+	for i := 0; i < 1; i++ {
+		for j := 0; j < 10; j++ {
+			logs[i] = append(logs[i], somewhatFreqOne, "\n")
+		}
 	}
 
-	frequentAll := generateRandomWord(4)
+
+	for i, log := range logs {
+		fmt.Printf("logs[%d]: %s", i, log)
+	}
+
+	// for log in logs
+	// upload log to machien i using endpoint
+
+
+	// grep -> compare results -c
+
+	// frequentAll := generateRandomWord(4)
 
 	// "vm2:8000/upload-to-test-log"
 	// vm -> write to test.log\
