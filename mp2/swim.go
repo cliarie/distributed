1b63f5c6c14a7755d1f78b3c224cf94313cb6b6a
alexy3 <alexy3@illinois.edu>
2024-09-24 17:42:55 -0500

    more work

--- mp2/swim.go
+++ mp2/swim.go
@@ -18,7 +18,7 @@ package main
 
 import (
 	"fmt"
-	// "math/rand"
+	"math/rand"
 	"net"
 	"sync"
 	"time"
@@ -33,8 +33,9 @@ var membershipList = map[string]Member{
 	"172.22.158.22:5000": {Status: "ALIVE"},
 }
 
-
-func ack(wg *sync.WaitGroup, addr *net.UDPAddr) {
+// currently this assumes all incoming UDPs are PINGs
+// but we also have to account for indirect PINGs
+func listener(wg *sync.WaitGroup, addr *net.UDPAddr) {
 	defer wg.Done() // Signal that this goroutine is done when it exits
 
 	conn, err := net.ListenUDP("udp", addr)
@@ -44,7 +45,7 @@ func ack(wg *sync.WaitGroup, addr *net.UDPAddr) {
 	}
 	defer conn.Close() // Close the connection when done
 
-	fmt.Printf("Listening for ACKs on %s\n", addr.String())
+	fmt.Printf("Listening on %s\n", addr.String())
 
 	buf := make([]byte, 1024)
 	for {
@@ -68,40 +69,14 @@ func ack(wg *sync.WaitGroup, addr *net.UDPAddr) {
 // Function to follow ping protocol for a specific address in a single cycle
 // and implement sucess and marking the node as failed
 // A separate function will create the random permutation list every n iterations
-func pingAddress(wg *sync.WaitGroup) {
+func processPingCycle(wg *sync.WaitGroup) {
 	defer wg.Done() // Signal that this goroutine is done when it exits
 
 	for {
 		// Choose a random address
 		address := "172.22.158.22:5000" // address will be given as an argument
-
-		// Attempt to send a ping
-		fmt.Printf("Sending PING to %s...\n", address)
-
-		// Create a UDP connection
-		conn, err := net.Dial("udp", address)
-		if err != nil {
-			fmt.Printf("Error dialing %s: %v\n", address, err)
-			time.Sleep(1 * time.Second) // Sleep before the next ping
-			continue
-		}
-		defer conn.Close()
-
-		// Send a PING message
-		_, err = conn.Write([]byte("PING"))
-		if err != nil {
-			fmt.Printf("Error sending PING to %s: %v\n", address, err)
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		// Set a deadline for receiving a response
-		conn.SetReadDeadline(time.Now().Add(2 * time.Second))
-
-		// Read the response
-		buf := make([]byte, 1024)
-		_, err = conn.Read(buf)
-		if err != nil {
+		res := pingSingleAddress(address)
+		if res != 0 {
 			fmt.Printf("No ACK from %s. Attempting indirect ping.\n", address)
 			// ping k random addresses that we have, asking them to ping for us
 			// We'll have to handle this differently in the ack. maybe smth like
@@ -111,9 +86,47 @@ func pingAddress(wg *sync.WaitGroup) {
 			// indirect ACK
 			// actually, i should have a single listening goroutine which sends the recieved messages to other
 			// channels to get processesed!
-			
 			// ask 3 random addressess to ping it for us
 			// "indirect ping ??"
+			nodesToPing := randomKNodes(3)
+			var indirectWg sync.WaitGroup
+			indirectACK := false
+			for _, nodeAddress := range nodesToPing {
+				indirectWg.Add(1)
+				go func(node string) {
+					defer indirectWg.Done()
+					conn, err := net.Dial("udp", node)
+					if err != nil {
+						fmt.Printf("Error dialing %s: %v\n", node, err)
+						return
+					}
+					defer conn.Close()
+					// Send a request to the node to ping the targetAddress
+					// TODO make listener handle for indirect ping requests
+					// ie PING vs PING <address>
+					request := fmt.Sprintf("PING %s", address)
+					_, err = conn.Write([]byte(request))
+					if err != nil {
+						return
+					}
+					// Wait for a response (ACK)
+					buf := make([]byte, 1024)
+					conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+					_, err = conn.Read(buf)
+					if err != nil {
+						return
+					}
+					
+					// TODO: add a mutex lock for this
+					indirectACK = true
+				}(nodeAddress)
+			}
+			indirectWg.Wait()
+			if indirectACK {
+                fmt.Printf("Received indirect ACK for %s\n", address)
+            } else {
+                fmt.Printf("No indirect ACK for %s. Marking node as failed.\n", address)
+            }
 		} else {
 			fmt.Printf("Received ACK from %s\n", address)
 		}
@@ -121,6 +134,67 @@ func pingAddress(wg *sync.WaitGroup) {
 		// Sleep for 1 second before the next ping
 		time.Sleep(1 * time.Second)
 	}
+}
+
+// function to ping a given address, and then return 0 if recieved ACK, 1 if no ACK
+func pingSingleAddress(address string) int {
+	// Attempt to send a ping
+	fmt.Printf("Sending PING to %s...\n", address)
+
+	// Create a UDP connection
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		fmt.Printf("Error dialing %s: %v\n", address, err)
+		return 1
+	}
+	defer conn.Close()
+
+	// Send a PING message
+	_, err = conn.Write([]byte("PING"))
+	if err != nil {
+		fmt.Printf("Error sending PING to %s: %v\n", address, err)
+		return 1
+	}
+
+	// Set a deadline for receiving a response
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+
+	// Read the response
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func randomKNodes(n int) []string {
+	keys := make([]string, 0, len(membershipList))
+	for key := range membershipList {
+		keys = append(keys, key)
+	}
+
+	// Set to track selected indices
+	selectedIndices := make(map[int]struct{})
+	var selectedNodes []string
+
+	rand.Seed(time.Now().UnixNano())
+
+	for len(selectedNodes) < n && len(selectedIndices) < len(keys) {
+		// Generate a random index
+		randomIndex := rand.Intn(len(keys))
+
+		// Check if this index has already been selected
+		if _, exists := selectedIndices[randomIndex]; !exists {
+			// Mark this index as selected
+			selectedIndices[randomIndex] = struct{}{}
+			// Append the corresponding member to the selectedNodes slice
+			selectedNodes = append(selectedNodes, keys[randomIndex])
+		}
+	}
+
+	return selectedNodes
 }
 
 func main() {
@@ -131,11 +205,11 @@ func main() {
 	}
 
 	wg.Add(1)
-	go ack(&wg, addr) // Start our ACK goroutine
+	go listener(&wg, addr) // Start our ACK goroutine
 
 
 	wg.Add(1)
-	go pingAddress(&wg)
+	go processPingCycle(&wg)
 
 	select {}
 }
